7c25bbe6a74ef596ae48b3468aadc0b7c4522b12
Chris.Brennan <brennanc@hotmail.com>
2024-01-27 10:27:01 -0500

    Sync

--- Python/CB_Module.py
+++ Python/CB_Module.py
@@ -76,7 +76,7 @@ def lazygit():
 			'git push'
 			]
 	for cmd in cmds:
-		print("EXECUTING: ==>" + cmd)
+		print("EXECUTING: ***** ==> " + cmd)
 		#os.system(cmd)
 		output = os.system(cmd)
 		#print(output)
