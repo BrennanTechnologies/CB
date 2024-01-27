9cbd488699fb4b17ce32c584e23fcbd01fca0332
Chris.Brennan <brennanc@hotmail.com>
2024-01-27 10:04:01 -0500

    Sync

--- Python/HelloWorld.py
+++ Python/HelloWorld.py
@@ -2,13 +2,16 @@ import sys
 
 print("Hello World!")
 
-import CB_Module
-module_name = "CB_Module"
+#import CB_Module
+import CB_Module as cb
 
+module_name = "CB_Module"
 if module_name in sys.modules:
-    print()
+    print("Found ME!!!")
 	#print(module_name + " was found!")
 else:
     print(module_name + " was NOT found!")
 
-CB_Module.lazygit()
+cb.write_log("New Log")
+
+cb.lazygit()
